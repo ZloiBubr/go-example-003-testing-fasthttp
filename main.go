173edf7295b50614af7efa1702978ea2c972d058
Siarhei Hladkou <shladkou@icloud.com>
2015-12-18 23:00:55 -0500

    more sophisticated calls

--- main.go
+++ main.go
@@ -1,45 +1,38 @@
 package main
 
 import (
-	"github.com/valyala/fasthttp"
 	"fmt"
+	"github.com/valyala/fasthttp"
 	"log"
 	"time"
 )
 
 var (
-	elapsed time.Duration;
+	elapsed time.Duration
 )
+
 func main() {
 
-/*	myHandler := &MyHandler{
-		foobar: "foobar",
-	}
-	fasthttp.ListenAndServe(":8080", myHandler.HandleFastHTTP)
-*/
-	// pass plain function to fasthttp
-	fasthttp.ListenAndServe(":8081", fastHTTPHandler)
-
-
-
+	fasthttp.ListenAndServe(":8081", requestHandler)
 }
 
-/*type MyHandler struct {
-	foobar string
-}
-
-// request handler in net/http style, i.e. method bound to MyHandler struct.
-func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
-	// notice that we may access MyHandler properties here - see h.foobar.
-	fmt.Fprintf(ctx, "Hello, world! Requested path is %q. Foobar is %q",
-		ctx.Path(), h.foobar)
-}
-*/
-// request handler in fasthttp style, i.e. just plain function.
-func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
-	start := time.Now()
+func fooHandler(ctx *fasthttp.RequestCtx) {
 	fmt.Fprintf(ctx, "Hi there! RequestURI is %q, elapsed = %s", ctx.RequestURI(), elapsed)
+}
+func barHandler(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "Hi there! RequestURI is %q, elapsed = %s", ctx.RequestURI(), elapsed)
+}
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	start := time.Now()
+	switch string(ctx.Path()) {
+	case "/foo":
+		fooHandler(ctx)
+	case "/bar":
+		barHandler(ctx)
+	default:
+		ctx.Error("Unsupported path", fasthttp.StatusNotFound)
+	}
 	elapsed = time.Since(start)
 	log.Printf("Processing took %s", elapsed)
 }
-
